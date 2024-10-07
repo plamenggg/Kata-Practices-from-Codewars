1ad2fa3cdcdaeb0e2a4f8b6e3fc3bd073767cbd3
plamenggg <plamen_ganchev_94@abv.bg>
2024-10-07 14:58:18 +0300

    New Kata

---
+++ 14.js
@@ -0,0 +1,10 @@
+scoreDolphins = (96 + 108 +89) / 3;
+scoreKoalas = (88 + 91 +110) /3;
+
+if (scoreDolphins > scoreKoalas) {
+    console.log('Dolphins win the trophy')
+} else if (scoreDolphins < scoreKoalas) {
+    console.log('Koalas win the trophy')
+} else if (scoreDolphins === scoreKoalas) {
+    console.log('Both win the trophy')
+}
