cf712652e9759681172cb0a5cceaedfec1f721a1
plamenggg <plamen_ganchev_94@abv.bg>
2024-10-07 09:37:58 +0300

    New Kata

--- 13.js
+++ 13.js
@@ -7,5 +7,16 @@ const heightJohn = 1.95;
 const BMIMark = massMark / (heightMark * heightMark);
 const BMIJohn = massJohn / (heightJohn * heightJohn);
  
-const markHigherBMI = BMIMark > BMIJohn;
 console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log("Mark's BMI is higher than John's!")
+} else {
+    console.log("John's BMI is higher than Mark's!")
+}
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}`);
+} else {
+    console.log(`John's ${BMIJohn} is higher than Mark's BMI ${BMIMark}`)
+}
