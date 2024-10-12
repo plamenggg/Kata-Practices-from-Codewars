3eb1f6470fcd25b083df2f33378ccfb49aac9b56
plamenggg <plamen_ganchev_94@abv.bg>
2024-10-12 17:22:42 +0300

    New Kata

--- 19.js
+++ 19.js
@@ -1,26 +1,28 @@
 const calcTip = function (bill) {
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-  }
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
 
-  const bills = [22,295,176,440,37,105,10,1100,86,52];
-  const tips = [];
-  const totals = [];
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
 
-  for (let i=0; i <= bills.length-1; i++) {
-    let tip = calcTip(bills[i]);
-    let total = bills[i] + tip;
-    tips.push(tip);
-    totals.push(total);
-  }
+for (let i = 0; i <= bills.length - 1; i++) {
+  let tip = calcTip(bills[i]);
+  let total = bills[i] + tip;
+  tips.push(tip);
+  totals.push(total);
+}
 
-  console.log(tips);
-  console.log(totals);
+console.log(tips);
+console.log(totals);
 
-  calcAverage = (arr) => {
-    return arr.reduce((acc, el) => {
-        acc+=el;
-        return acc;
-    },0) /arr.length
-  }
+calcAverage = (arr) => {
+  return (
+    arr.reduce((acc, el) => {
+      acc += el;
+      return acc;
+    }, 0) / arr.length
+  );
+};
 
-  console.log(calcAverage(totals))
+console.log(calcAverage(totals));
