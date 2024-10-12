90cba85f2f9dd6510b9318e0bae651832d30e819
plamenggg <plamen_ganchev_94@abv.bg>
2024-10-12 11:31:01 +0300

    New Kata

---
+++ 16.js
@@ -0,0 +1,17 @@
+const calcAverage = (one,two,three) =>  (one+two+three) /3;
+
+function checkWinner(avgDolphins, avgKoalas) {
+    if (avgDolphins >= avgKoalas *2) {
+        return `Dolphins win (${avgDolphins} vs. ${avgKoalas})`
+    }
+    if (avgKoalas >= avgDolphins*2) {
+        return `Koalas win (${avgKoalas} vs. ${avgDolphins})`
+    }
+    return 'No team wins...'
+}
+
+const scoreDolphins = calcAverage(44,23,71);
+const scoreKoalas = calcAverage(65,54,49);
+
+console.log(checkWinner(scoreDolphins, scoreKolalas))
+
