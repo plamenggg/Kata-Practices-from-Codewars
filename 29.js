7d6de3ef08bbcf229afb85d0531d7a0a5d8c191d
plamenggg <plamen_ganchev_94@abv.bg>
2024-10-30 20:40:58 +0200

    New Kata

--- 29.js
+++ 29.js
@@ -16,3 +16,29 @@ const createBooking = function (
 createBooking("LH132");
 createBooking("lb321", 24);
 console.log(bookings);
+
+const oneWord = function (str) {
+  return str.replaceAll(" ", "").toLowerCase();
+};
+
+console.log(oneWord("kO OOKK"));
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+console.log(upperFirstWord("hello goodbye"));
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeter = greet("Hey");
+greeter("Joe");
+
+const greet2 = (greeting) => (name2) => console.log(`${greeting} ${name2}`);
+
+greet2("Hello")("Steve");
